ca40fba97acef872f127da238582b6b256f7f86b
lucky-KRMU <2501730142@krmu.edu.in>
2026-02-04 06:39:33 +0000

    add form variables in js

--- logic.js
+++ logic.js
@@ -1,3 +1,14 @@
 let form = document.getElementsByTagName("form");
 console.log(form);
+
+let title = document.getElementById("title");
+let date = document.getElementById("date");
+let category = document.getElementById("category");
+
+let titleValue = title.value;
+let dateValue = date.value;
+let categoryValue = category.value;
+
+
+
     
