998bec2bb44cdffe8a02dd83646705331864f008
lucky-KRMU <2501730142@krmu.edu.in>
2026-02-04 06:55:15 +0000

    add card add functionality in js

--- logic.js
+++ logic.js
@@ -4,15 +4,40 @@ let title = document.getElementById("title");
 let date = document.getElementById("date");
 let category = document.getElementById("category");
 let description = document.getElementById("Description");
+let submitBtn = document.getElementById("submit-btn");
+
+
+let gridCard = document.getElementById("grid-card");
+
 
 let titleValue = title.value;
 let dateValue = date.value;
 let categoryValue = category.value;
+let descriptionValue = description.value;
+
+let gridCardContent = gridCard.innerHTML;
+
 
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
-    title.value = '';
-    date.value = '';
-    description.value = '';
+    console.log(gridCardContent)
+
+    
+
+    // title.value = '';
+    // date.value = '';
+    // description.value = '';
+})
+
+submitBtn.addEventListener('click', ()=>{
+    let card = `<div class="card">
+                    <h4>${titleValue}</h4>
+                    <p class="date">&#128197; ${dateValue}</p>
+                    <br>
+                    <label>${categoryValue}</label>
+                    <p class="des">${descriptionValue}</p>
+                </div>`;
+
+    gridCard.innerHTML += card;
 })
