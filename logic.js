23f735398c41a467051bc3c720368998df9a0a72
lucky-KRMU <2501730142@krmu.edu.in>
2026-02-04 06:45:17 +0000

    add form submit btn functionality (prevent default)

--- logic.js
+++ logic.js
@@ -1,14 +1,18 @@
-let form = document.getElementsByTagName("form");
-console.log(form);
+let form = document.getElementById("event-form");
 
 let title = document.getElementById("title");
 let date = document.getElementById("date");
 let category = document.getElementById("category");
+let description = document.getElementById("Description");
 
 let titleValue = title.value;
 let dateValue = date.value;
 let categoryValue = category.value;
 
 
-
-    
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    title.value = '';
+    date.value = '';
+    description.value = '';
+})
