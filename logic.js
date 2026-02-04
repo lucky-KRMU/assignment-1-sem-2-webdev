483f829e529d62a7de5d8943bbab4bb722b90694
lucky-KRMU <2501730142@krmu.edu.in>
2026-02-04 15:52:48 +0000

    add del logic(partially)

--- logic.js
+++ logic.js
@@ -9,8 +9,10 @@ let submitBtn = document.getElementById("submit-btn");
 
 let gridCard = document.getElementById("grid-card");
 
-let cardArray = [];
+let cancelBtn = document.querySelectorAll(".cancel-btn");
 
+let cardArray = [];
+let cardDetails = [];
 
 let titleValue = title.value;
 let dateValue = date.value;
@@ -19,25 +21,115 @@ let descriptionValue = description.value;
 
 let gridCardContent = gridCard.innerHTML;
 
+let key = 0;
+let head = 0;
 
+let newArray = [];
+
+let card;
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
-    console.log(gridCardContent)
+    // console.log(gridCardContent);
 })
 
-submitBtn.addEventListener('click', ()=>{
-    let card = `<div class="card">
-                    <h4>${titleValue}</h4>
-                    <p class="date">&#128197; ${dateValue}</p>
-                    <br>
-                    <label>${categoryValue}</label>
-                    <p class="des">${descriptionValue}</p>
-                </div>`;
-    cardArray.push(card)
+const display = () => {
     gridCard.innerHTML = '';
-    cardArray.map((e)=> gridCard.innerHTML+=e)
+    // cardArray.map((e)=> gridCard.innerHTML+=e);
+    Object.entries(cardObj).map(([key,value]) => gridCard.innerHTML+=value);
+}
+
+const populate = () => {
+    
+    key = 0
+
+    cardArray = [];
+    
+    cardDetails.forEach(card => {
+        let cardTemplate = `<div class="card">
+        <button class="cancel-btn" id="${key}">&#x274C;</button>
+        <h4>${card.title}</h4>
+        <p class="date">&#128197; ${card.date}</p>
+        <br>
+        <label>${card.category}</label>
+        <p class="des">${card.description}</p>
+        </div>`;
+        cardArray.push(cardTemplate);
+        key+=1;
+    })
+    
+    
+    gridCard.innerHTML = '';
+    cardArray.map((e)=>{
+        gridCard.innerHTML += e;
+    })
+}
+
+submitBtn.addEventListener('click', ()=>{
+
+    titleValue = title.value;
+    dateValue = date.value;
+    categoryValue = category.value;
+    descriptionValue = description.value;
+
+    let cardObj = {
+        "title":titleValue,
+        "date":dateValue,
+        "category":categoryValue,
+        "description":descriptionValue,
+    }
+
+    card = `<div class="card">
+                    <button class="cancel-btn" id="${key}">&#x274C;</button>
+                    <h4>${cardObj.title}</h4>
+                    <p class="date">&#128197; ${cardObj.date}</p>
+                    <br>
+                    <label>${cardObj.category}</label>
+                    <p class="des">${cardObj.description}</p>
+                </div>`;
+    // cardArray.push(card);
+    cardDetails.push(cardObj);
+    console.log(cardDetails)
+    populate();
+    // key+=1;
+    // gridCard.innerHTML+=card;
+    // display();
+    // console.log(cardObj)
+
+    cancelBtn = document.querySelectorAll(".cancel-btn");
+    console.log(cancelBtn);
+    // cancelBtn.forEach(btn => {
+    //     btn.addEventListener('click', (e)=>{
+    //         console.log(btn.id);
+    //         // head = String(btn.id)
+    //         // cardArray.splice(head, 1);
+    //         // delete cardObj.head
+    //         // console.log(updatedCardArray);
+    //         // console.log(cardArray);
+    //         // display();
+    //         // e.target.parentElement.remove();
+    //         cardDetails.splice(btn.id, 1);
+    //         populate();
+    //     })
+    // })
+    cancelBtnFunc();
+    
     // title.value = '';
     // date.value = '';
     // description.value = '';
 })
+
+cancelBtn = document.querySelectorAll(".cancel-btn");
+const cancelBtnFunc = () =>{
+    cancelBtn.forEach(btn => {
+        btn.addEventListener('click', (e)=>{
+            cardDetails.splice(btn.id, 1);
+            populate();
+        })
+    })
+}
+
+
+cancelBtnFunc();
+
+console.log(cancelBtn);
