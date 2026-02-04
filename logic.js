717d9b43566b4534391cf53abb0684b626e3a6b1
lucky-KRMU <2501730142@krmu.edu.in>
2026-02-04 16:18:12 +0000

    made code efficient

--- logic.js
+++ logic.js
@@ -1,40 +1,20 @@
-let form = document.getElementById("event-form");
+let form = document.getElementById("event-form"); // targeting the form
 
-let title = document.getElementById("title");
-let date = document.getElementById("date");
-let category = document.getElementById("category");
-let description = document.getElementById("Description");
-let submitBtn = document.getElementById("submit-btn");
+let title = document.getElementById("title"); // targeting the title
+let date = document.getElementById("date"); // targeting the date
+let category = document.getElementById("category"); // targeting the category
+let description = document.getElementById("Description"); // targeting the description
 
 
-let gridCard = document.getElementById("grid-card");
+let gridCard = document.getElementById("grid-card"); // targeting the card grid
 
-let cancelBtn = document.querySelectorAll(".cancel-btn");
 
-let cardArray = [];
-let cardDetails = [];
+let cardDetails = [];   // array to store card object details
 
-let titleValue = title.value;
-let dateValue = date.value;
-let categoryValue = category.value;
-let descriptionValue = description.value;
 
-let gridCardContent = gridCard.innerHTML;
-
-let key = 0;
-let head = 0;
-
-let newArray = [];
-
-let card;
-
-const display = () => {
-    gridCard.innerHTML = '';
-    // cardArray.map((e)=> gridCard.innerHTML+=e);
-    Object.entries(cardObj).map(([key,value]) => gridCard.innerHTML+=value);
-}
-
+// function to populate the cards
 const populate = () => {
+
     gridCard.innerHTML = '';
     
     
@@ -47,15 +27,11 @@ const populate = () => {
         <label>${card.category}</label>
         <p class="des">${card.description}</p>
         </div>`;
-        key+=1;
     })
-    
-    
-    // cardArray.map((e)=>{
-    //     gridCard.innerHTML += e;
-    // })
+
 }
 
+//event listener to add event and update the card list (population)
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
 
@@ -73,7 +49,7 @@ form.addEventListener('submit', (e)=>{
     description.value = '';
 })
 
-
+//event listener to delete the cards
 gridCard.addEventListener( 'click', (e)=>{
     if (e.target.classList.contains("cancel-btn")) {
         const index = e.target.dataset.index;
@@ -83,5 +59,3 @@ gridCard.addEventListener( 'click', (e)=>{
 })
 
 
-
-console.log(cancelBtn);
